eb2b9e61bea13f30419a8234ddb17f40d4456d65
Shahan <shahan.khaan@gmail.com>
2025-04-05 23:12:59 +0500

    fix: update gitsigns and neotree config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,20 +1,13 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    signs_staged = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
+  { 'tpope/vim-fugitive' },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+
+      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<leader>gt', ':Gitsigns preview_hunk_line_blame<CR>', {})
+    end,
   },
 }
